3c7dbfe536baba6f103d2dca817d93128b592df8
s4nthosh <santhoshshr43@gmail.com>
2023-03-07 23:36:38 +0530

    update

--- booking/settings.py
+++ booking/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'booking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME':BASE_DIR/'db.sqlite3',
     }
 }
