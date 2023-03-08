6fc5766482c8bc6748aebfc78192e8bea1755d5c
s4nthosh <101450090+s4nthosh@users.noreply.github.com>
2023-03-08 13:31:09 +0530

    update

--- booking/settings.py
+++ booking/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,7 +123,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/uploadimg/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'uploadimg')
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    STATIC_DIR,
 ]
 
 
